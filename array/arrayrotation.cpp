3394efc4fefacdc0f449916c6ce4e7e89ee466c0
vansh kabra <134841334+VANSH3104@users.noreply.github.com>
2024-09-19 21:53:43 +0530

    Update arrayrotation.cpp

--- array/arrayrotation.cpp
+++ array/arrayrotation.cpp
@@ -17,3 +17,18 @@ vector<int> kRotate(vector<int> a, int k){
     return b;
     
 }
+//methord 2
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> kRotate(vector<int> a, int k){
+    // your code  goes here
+    int n = a.size();
+    k = k%n;
+    reverse(a.begin(), a.begin()+n-k);
+    reverse(a.begin()+n-k, a.end());
+    reverse(a.begin(), a.end());
+    
+    return a;
+    
+}
